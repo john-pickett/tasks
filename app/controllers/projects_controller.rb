d5db8921a08a39b2ec68ab61d5d7402bbc44ccc4
John P <john.pickett@gmail.com>
2017-05-02 08:38:36 -0500

    updates to projects index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @tasks = Task.where(project_id: @project.id)
   end
 
   # GET /projects/new
